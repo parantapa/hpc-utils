fd2e209914fbc756db3e5ec58f878cf62bb4514f
Parantapa Bhattacharya <pb@parantapa.net>
2025-12-08 16:19:00 -0500

    Don't use find_dependency in -config.cmake

--- cmake/hpc-utils-config.cmake
+++ cmake/hpc-utils-config.cmake
@@ -1,7 +1,7 @@
-include(CMakeFindDependencyMacro)
+# include(CMakeFindDependencyMacro)
 
-find_dependency(OpenMP)
-find_dependency(fmt)
-find_dependency(phmap)
+# find_dependency(OpenMP)
+# find_dependency(fmt)
+# find_dependency(phmap)
 
 include("${CMAKE_CURRENT_LIST_DIR}/hpc-utils-targets.cmake")
